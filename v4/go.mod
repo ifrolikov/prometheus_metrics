13f556d636b3a8efc6e13ea1cf04dafc3ed2bb34
frolikov <frolikov@tutu.ru>
2021-03-24 10:53:49 +0300

    grpc interceptors

--- v4/go.mod
+++ v4/go.mod
@@ -3,12 +3,13 @@ module github.com/ifrolikov/prometheus_metrics/v4
 go 1.14
 
 require (
-	github.com/golang/protobuf v1.3.5 // indirect
 	github.com/grafana-tools/sdk v0.0.0-20200520194427-14a3f1e087f0
+	github.com/iancoleman/strcase v0.1.3
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/pkg/errors v0.9.1
 	github.com/prometheus/client_golang v1.5.1
 	github.com/prometheus/procfs v0.0.11 // indirect
 	golang.org/x/sys v0.0.0-20200420163511-1957bb5e6d1f // indirect
+	google.golang.org/grpc v1.36.0
 )
---
+++ v4/grpc_interceptor/interceptor.go
@@ -0,0 +1,49 @@
+package grpcsinterceptor
+
+import (
+	"context"
+	"github.com/iancoleman/strcase"
+	"github.com/ifrolikov/prometheus_metrics/v4/interfaces"
+	"google.golang.org/grpc"
+	"path"
+	"regexp"
+	"time"
+)
+
+var re = regexp.MustCompile(`[^\w\d]+`)
+
+func NewMetricsTimerUnaryInterceptor(collector interfaces.Collector) grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+		startTime := time.Now()
+
+		resp, err := handler(ctx, req)
+
+		method := path.Base(info.FullMethod)
+		metricName := strcase.ToSnake(re.ReplaceAllString(method, "_"))
+
+		hasError := "false"
+		if err != nil {
+			hasError = "true"
+		}
+		_ = collector.ObserveTimer(metricName, startTime, map[string]string{"has_error": hasError})
+		return resp, err
+	}
+}
+
+func NewMetricsTimerStreamInterceptor(collector interfaces.Collector) grpc.StreamServerInterceptor {
+	return func(srv interface{}, stream grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+		startTime := time.Now()
+
+		err := handler(srv, stream)
+
+		method := path.Base(info.FullMethod)
+		metricName := strcase.ToSnake(re.ReplaceAllString(method, "_"))
+
+		hasError := "false"
+		if err != nil {
+			hasError = "true"
+		}
+		_ = collector.ObserveTimer(metricName, startTime, map[string]string{"has_error": hasError})
+		return err
+	}
+}
