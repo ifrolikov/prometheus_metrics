b3688660e9146b7287820bdc22a43d0c6634ee22
frolikov <frolikov@tutu.ru>
2022-11-07 14:36:15 +0300

    remove grafana grafs

--- v4/collector.go
+++ v4/collector.go
@@ -1,27 +1,16 @@
 package prometheus_metrics
 
 import (
-	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
-	"github.com/ifrolikov/prometheus_metrics/v4/grafana"
 	"github.com/prometheus/client_golang/prometheus"
 	"sort"
 	"sync"
 	"time"
 )
 
-const (
-	GRAPH_TYPE_GAUGE     graphType = "gauge"
-	GRAPH_TYPE_HISTOGRAM graphType = "histogram"
-	GRAPH_TYPE_COUNTER   graphType = "counter"
-	GRAPH_TYPE_TIMER     graphType = "timer"
-)
-
 var globalCollector *Collector
-
-type graphType string
 
 type Collector struct {
 	podName                   string
@@ -33,26 +22,18 @@ type Collector struct {
 	namespace                 string
 	subsystem                 string
 	mtx                       *sync.Mutex
-	grafanaService            *grafana.Service
 	grafanaDashboard          *string
 	histogramMetricsMap       map[string]*prometheus.HistogramVec
 	histogramMetricsLabelsMap map[string][]string
 	gaugeMetricsLabelsMap     map[string][]string
 }
 
-func NewCollector(podName string, namespace string, subsystem string, grafanaInitData *grafana.InitData) *Collector {
+func NewCollector(podName string, namespace string, subsystem string) *Collector {
 	collector := &Collector{
 		podName:   podName,
 		namespace: namespace,
 		subsystem: subsystem,
 		mtx:       &sync.Mutex{},
-	}
-	if grafanaInitData != nil {
-		collector.grafanaService = grafana.NewService(
-			grafanaInitData.ApiUrl,
-			grafanaInitData.AuthKey,
-			grafanaInitData.DataSource)
-		collector.grafanaDashboard = &grafanaInitData.DefaultDashboard
 	}
 	return collector
 }
@@ -61,8 +42,8 @@ func GetGlobalCollector() *Collector {
 	return globalCollector
 }
 
-func InitGlobalCollector(podName string, namespace string, subsystem string, grafanaInitData *grafana.InitData) *Collector {
-	globalCollector = NewCollector(podName, namespace, subsystem, grafanaInitData)
+func InitGlobalCollector(podName string, namespace string, subsystem string) *Collector {
+	globalCollector = NewCollector(podName, namespace, subsystem)
 	return globalCollector
 }
 
@@ -70,35 +51,18 @@ func (c *Collector) ObserveTimer(name string, startTime time.Time, labels map[st
 	defer c.mtx.Unlock()
 	c.mtx.Lock()
 
-	specialLabels := map[string]string{}
-	if labels == nil {
-		labels = map[string]string{}
-	} else {
-		labels, specialLabels = c.separateSpecialLabels(labels)
-	}
-
 	err := c.initTimerIfNotExist(name, labels)
 	if err != nil {
 		return err
 	}
 	c.timeMetricsMap[name].With(labels).Observe(float64(time.Since(startTime)))
 
-	err = c.tryToCreateGrafanaGraph(specialLabels, GRAPH_TYPE_TIMER, name, context.TODO())
-	if err != nil {
-		return err
-	}
 	return nil
 }
 
 func (c *Collector) ObserveHistogram(name string, startTime time.Time, labels map[string]string) error {
 	defer c.mtx.Unlock()
 	c.mtx.Lock()
-
-	if labels == nil {
-		labels = map[string]string{}
-	} else {
-		labels, _ = c.separateSpecialLabels(labels)
-	}
 
 	err := c.initHistogramIfNotExist(name, labels)
 	if err != nil {
@@ -114,35 +78,18 @@ func (c *Collector) ObserveCounter(name string, inc int, labels map[string]strin
 	defer c.mtx.Unlock()
 	c.mtx.Lock()
 
-	specialLabels := map[string]string{}
-	if labels == nil {
-		labels = map[string]string{}
-	} else {
-		labels, specialLabels = c.separateSpecialLabels(labels)
-	}
-
 	err := c.initCounterIfNotExist(name, labels)
 	if err != nil {
 		return err
 	}
 	c.counterMetricsMap[name].With(labels).Add(float64(inc))
 
-	err = c.tryToCreateGrafanaGraph(specialLabels, GRAPH_TYPE_COUNTER, name, context.TODO())
-	if err != nil {
-		return err
-	}
 	return nil
 }
 
 func (c *Collector) ObserveGauge(name string, inc int, labels map[string]string) error {
 	defer c.mtx.Unlock()
 	c.mtx.Lock()
-
-	if labels == nil {
-		labels = map[string]string{}
-	} else {
-		labels, _ = c.separateSpecialLabels(labels)
-	}
 
 	err := c.initGaugeIfNotExist(name, labels)
 	if err != nil {
@@ -305,55 +252,6 @@ func (c *Collector) initGaugeIfNotExist(name string, labels map[string]string) e
 				"requested labels: %s",
 				marshaledCurrentMetricLabels,
 				marshaledRequestedMetricLabels))
-		}
-	}
-	return nil
-}
-
-func (c *Collector) separateSpecialLabels(labels map[string]string) (map[string]string, map[string]string) {
-	specialLabels := map[string]string{}
-	customLabels := map[string]string{}
-
-	for key, val := range labels {
-		isSpecialLabel := false
-		for _, label := range grafana.GrafanaLabels {
-			if string(label) == key {
-				specialLabels[key] = val
-				isSpecialLabel = true
-				break
-			}
-		}
-		if !isSpecialLabel {
-			customLabels[key] = val
-		}
-	}
-	return customLabels, specialLabels
-}
-
-func (c *Collector) tryToCreateGrafanaGraph(labels map[string]string,
-	sendMethodType graphType,
-	metricName string,
-	ctx context.Context) error {
-	if graphTitle, ok := labels[string(grafana.LABEL_TITLE)]; ok {
-		if c.grafanaService == nil {
-			return errors.New("grafana service is not initialized")
-		}
-		dashboard, isCustomDashboard := labels[string(grafana.LABEL_DASHBOARD_TITLE)]
-		if !isCustomDashboard {
-			dashboard = *c.grafanaDashboard
-		}
-		var datasource *string
-		if val, isCustomDatasource := labels[string(grafana.LABEL_DASHBOARD_TITLE)]; isCustomDatasource {
-			datasource = &val
-		}
-
-		switch sendMethodType {
-		case GRAPH_TYPE_COUNTER:
-			return c.grafanaService.PushCounterGraph(dashboard, metricName, graphTitle, c.namespace, c.subsystem, ctx, datasource)
-		case GRAPH_TYPE_TIMER:
-			return c.grafanaService.PushTimerGraph(dashboard, metricName, graphTitle, c.namespace, c.subsystem, ctx, datasource)
-		default:
-			return errors.New(fmt.Sprintf("unknown graph type: %s", string(sendMethodType)))
 		}
 	}
 	return nil
